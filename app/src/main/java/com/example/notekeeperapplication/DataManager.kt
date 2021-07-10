15bbea1c16fc8f52d3854c6d14f96525c1e8d5b2
Gmatieso <matiesogeoffrey@gmail.com>
2021-07-10 04:07:02 +0300

    added a function initializer course

--- app/src/main/java/com/example/notekeeperapplication/DataManager.kt
+++ app/src/main/java/com/example/notekeeperapplication/DataManager.kt
@@ -3,4 +3,23 @@ package com.example.notekeeperapplication
 class DataManager {
     val courses = HashMap<String,CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses ()
+    }
+
+    private fun initializeCourses () {
+        var course = CourseInfo("android_intents","android programming with intents")
+        courses.set(course.courseid,course)
+
+        course = CourseInfo("android_async","Android Async programming and services")
+        courses.set(course.courseid,course)
+
+        course = CourseInfo("java_lang","Java Fundamentals:The Java language")
+        courses.set(course.courseid,course)
+
+        course = CourseInfo("java_core","Java Fundamentals:The core Plartform")
+        courses.set(course.courseid,course)
+
+    }
 }
