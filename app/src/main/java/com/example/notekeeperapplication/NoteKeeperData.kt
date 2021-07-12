222621a64d28331a90f07f16b83ee2f1215ae29a
Gmatieso <matiesogeoffrey@gmail.com>
2021-07-12 05:43:41 +0300

    added a return method to the courseInfo class

--- app/src/main/java/com/example/notekeeperapplication/NoteKeeperData.kt
+++ app/src/main/java/com/example/notekeeperapplication/NoteKeeperData.kt
@@ -1,6 +1,11 @@
 package com.example.notekeeperapplication
 
-class CourseInfo(val courseid:String, val title:String)
+class CourseInfo(val courseid:String, val title:String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
 class NoteInfo(var course:CourseInfo, var title: String, var text:String)
 
 
